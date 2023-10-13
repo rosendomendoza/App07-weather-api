4de36d1831b30b956836b060e6cd6e7af6ec5d64
ROSENDO MENDOZA <rosendo.arturo@gmail.com>
2023-10-13 13:00:37 +0200

    Day 28: Initial Commit with: 1. make Flask website 2. Make API wheather Date

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask("Website")
+app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return (render_template("home.html"))
+
+@app.route("/api/v1/<station>/<date>")
+def api(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+
+if __name__ == "__main__":
+    app.run(debug=True)
