0e8775c01fda44b7cf973210f8fccc704ed64d95
ROSENDO MENDOZA <rosendo.arturo@gmail.com>
2023-10-14 22:00:07 +0200

    Day 31: Show  Data station Table in the home page

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from flask import Flask, render_template
 from search_temp import search_temp
+import pandas as pd
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return (render_template("home.html"))
+    stations = pd.read_csv("data_small/stations.txt", skiprows=17)
+    stations = stations[["STAID", "STANAME                                 "]]
+    return render_template("home.html", data=stations.to_html())
 
 @app.route("/api/v1/<station>/<date>")
 def api(station, date):
-    temperature = search_temp(station, date)
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+
+    temperature = str(df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10)
     return {"station": station,
             "date": date,
             "temperature": temperature}
